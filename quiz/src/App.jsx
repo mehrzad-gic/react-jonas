bbabc1c617f8e27a3d79555244f642b4e19ef9ff
Mehrzad <mehrzad20061384@gmail.com>
2025-02-15 19:47:05 +0330

    initial states and reducer and fetch ✅

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,27 +1,131 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
+import { useReducer, useEffect } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Loader from './components/Loader';
 
+const initialState = {
+  questions: null,
+  selectedQuestion: null,
+  status: null, // loading, error, ready, active, finished
+  errorMessage: null,
+};
+
+function reducer(state, action) {
+
+  switch (action.type) {
+
+    case "loading":
+      return {
+        ...state,
+        status: action.payload ? "loading" : "finished",
+      };
+
+    case "error":
+      return {
+        ...state,
+        status: "error",
+        errorMessage: action.payload,
+      };
+
+    case "ready":
+      return {
+        ...state,
+        status: "ready",
+        questions: action.payload,
+      };
+
+    case "active":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    case "finished":
+      return {
+        ...state,
+        status: "finished",
+      };
+
+    default:
+      return {
+        ...state,
+        status: "error",
+        errorMessage: "Internal Server Error [Action unknown]",
+      };
+  }
+
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+
+    const abortController = new AbortController();
+
+    async function fetchData() {
+
+      try {
+
+        dispatch({ type: 'loading', payload: true });
+        const data = await fetch(`http://localhost:5000/questions`, {
+          signal: abortController.signal,
+        });
+        if (!data.ok) {
+          dispatch({ type: 'error', payload: 'Something went wrong while fetching data' });
+          return;
+        }
+
+        const res = await data.json();
+        if (!res || res.length < 1) {
+          dispatch({ type: 'error', payload: 'Data is not valid' });
+          return;
+        }
+
+        dispatch({ type: 'ready', payload: res });
+
+      } catch (e) {
+        if (e.name !== 'AbortError') {
+          dispatch({ type: 'error', payload: e.message });
+        }
+      } finally {
+        dispatch({ type: 'loading', payload: false });
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      abortController.abort();
+    };
+
+  }, []);
 
   return (
 
     <>
 
-      <div className='app'>
+      <div className="app">
 
         <Header />
 
-        <Main >
-          hi
-        </Main>
-      
+        {state.status === 'loading' && <Loader />}
+
+        {state.status === 'error' && <p>{state.errorMessage}</p>}
+
+        {state.status === 'ready' && (
+          <Main>
+            
+          </Main>
+        )}
+
       </div>
 
     </>
-  )
+
+  );
 
 }
 
-export default App
+export default App;
